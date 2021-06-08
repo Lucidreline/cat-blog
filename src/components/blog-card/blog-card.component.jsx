17954ea44e3a72d4f1cbca76d76369974b4a0f60
Lucidreline <casta.ma502@gmail.com>
2021-06-08 15:54:05 -0700

    Implementing global variables from App.scss.

--- src/components/blog-card/blog-card.component.jsx
+++ src/components/blog-card/blog-card.component.jsx
@@ -1,37 +1,36 @@
-import React from 'react';
+import React from "react"
 
-import './blog-card.styles.scss'
+import "./blog-card.styles.scss"
 
 const BlogCard = () => {
   return (
     <article className='blog-card'>
-      <div className="card-img"></div>
-      <div className="card-info">
-        <div className="content-and-author">
-          <div className="content">
-            <h2 className="blog-card-title">Ipsum Dolor Sit</h2>
-            <p className="blog-card-content-preview">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, est laudantium necessitatibus.
+      <div className='card-img'></div>
+      <div className='card-info'>
+        <div className='content-and-author'>
+          <div className='content'>
+            <h3 className='blog-card-title'>Ipsum Dolor Sit</h3>
+            <p className='blog-card-content-preview'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Possimus, est laudantium necessitatibus.
             </p>
           </div>
-          <div className="author-container"> 
-            <p className="author">by Manny</p>
+          <div className='author-container'>
+            <span className='author'>by Manny</span>
           </div>
         </div>
-        <div className="likes-and-share">
-          <div className="share-btn-container">
-            <div className="share-btn"></div>
+        <div className='likes-and-share'>
+          <div className='share-btn-container'>
+            <div className='share-btn'></div>
           </div>
-          <div className="likes-btn-container">
-            <span className="likes-counter">5</span>
-            <div className="likes-btn"></div>
+          <div className='likes-btn-container'>
+            <span className='likes-counter'>5</span>
+            <div className='likes-btn'></div>
           </div>
         </div>
       </div>
     </article>
-  );
-};
+  )
+}
 
-
-
-export default BlogCard;
+export default BlogCard
--- src/components/nav/nav.component.jsx
+++ src/components/nav/nav.component.jsx
@@ -4,20 +4,22 @@ import "./nav.styles.scss"
 
 const Nav = () => {
   return (
-    <nav id='nav-bar'>
-      <div id='logo-container'>
-        <h1 id='logo'>CA</h1>
-      </div>
-      <div className='nav-links'>
-        <ul>
-          <li>Home</li>
-          <li>Sign In</li>
-          <li>Sign Up</li>
-          <li>About</li>
-          <li>gotMilk?</li>
-        </ul>
-      </div>
-    </nav>
+    <div className='nav-bar-container'>
+      <nav id='nav-bar'>
+        <div id='logo-container'>
+          <h1 id='logo'>CB</h1>
+        </div>
+        <div className='nav-links'>
+          <ul>
+            <li className='active-link'>Home</li>
+            <li>Sign In</li>
+            <li>Sign Up</li>
+            <li>About</li>
+            <li>gotMilk?</li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   )
 }
 
