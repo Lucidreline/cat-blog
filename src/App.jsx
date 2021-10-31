488b2ba36443be6ae15050750154fa314cbd1a64
Lucidreline <casta.ma502@gmail.com>
2021-10-31 08:36:53 +0000

    Refreshing will no longer log out the user.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import "./App.scss";
 import Nav from "./components/nav/nav.component";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { HomePage } from "./components/pages/homepage/homepage.component";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { SignInPage } from "./components/pages/sign-in-page/sign-in-page.component";
 import { SignUpPage } from "./components/pages/sign-up-page/sign-up-page.component";
 
+import { SERVER_URL } from "./config";
+
 function App() {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [currentUser, setCurrentUser] = useState();
+  useEffect(() => {
+    const fetchData = async () => {
+      const blogPostResults = await axios.get(`${SERVER_URL}/users/who-am-i`, {
+        withCredentials: true,
+      });
+      if ("" === blogPostResults.data) setCurrentUser(null);
+      else setCurrentUser(blogPostResults.data);
+    };
+    fetchData();
+  }, []);
   return (
     <Router>
       <div className="app">
